13fd9aa022750ebb9a7dc6f67d1e28ba2b8863a4
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-06-08 19:32:41 +0530

    navbar improvement

--- src/complete.jsx
+++ src/complete.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import NavBar from './components/navbar/Navbar.jsx'
+import NavBar from './components/navbar/NavBar.jsx'
 import Footer from './components/footer/Footer.jsx'
 import Landing_page from './components/landing_page/Landing_page.jsx'
 
 function Complete() {
   return (
     <>
-        <NavBar/>
-        <Landing_page/>
-        <Footer/>
+      <NavBar/>
+      <Landing_page/>
+      <Footer/>
     </>
   )
 }
